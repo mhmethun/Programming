ff6d80692942d919975ea2f7e9e5071370a219cf
Md. Kamruzzaman <mhmethun@live.com>
2014-08-15 15:03:33 +0600

    New changes

---
+++ DotNet/WCFFirstTry/WCFGetMessage/WCFGetMessageService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFGetMessage
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCFGetMessageService" in both code and config file together.
+    public class WCFGetMessageService : IWCFGetMessageService
+    {
+
+        #region IWCFGetMessageService Members
+
+        public string getMessage(string name)
+        {
+            return "Hello " + name;
+        }
+
+        #endregion
+    }
+}
---
+++ DotNet/WCFFirstTry/WCFGetMessageServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCFGetMessageServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            using(ServiceHost server = new ServiceHost(typeof(WCFGetMessage.WCFGetMessageService))){
+                try
+                {
+                    server.Open();
+                }
+                catch (CommunicationException ex)
+                {
+                    Console.WriteLine("Error in open server connection: " + ex.Message.ToString());
+                }
+                finally {
+                    Console.WriteLine("Server is starting @ " + DateTime.Now.ToString());
+                    Console.ReadLine();
+                }
+               
+            }
+        }
+    }
+}
