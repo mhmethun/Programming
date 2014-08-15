706a9c0327b6e41f90f17baea47d8409347b3ea3
Md. Kamruzzaman <mhmethun@live.com>
2014-08-15 15:20:18 +0600

    client part

---
+++ DotNet/WCFFirstTry/WCFGetMessageClient/ClientForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WCFGetMessageClient
+{
+    public partial class ClientForm : Form
+    {
+        public ClientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WCFGetMessageServerService.WCFGetMessageServiceClient client = new WCFGetMessageServerService.WCFGetMessageServiceClient("BasicHttpBinding_IWCFGetMessageService");
+            nameLabel.Text = client.getMessage(nameTextBox.Text);
+
+        }
+    }
+}
